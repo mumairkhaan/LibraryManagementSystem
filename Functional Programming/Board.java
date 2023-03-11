2cb9b72d7df67cf7fb68fd25a3b37c475db38de1
mumairkhaan <99419823+mumairkhaan@users.noreply.github.com>
2023-03-11 16:10:11 +0100

    Update Board.java

--- Functional Programming/Board.java
+++ Functional Programming/Board.java
@@ -11,10 +11,11 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Board {
-    private final PVector<Optional<Player>> board;
-    private final int rowSize;
+    private final PVector<Optional<Player>> board;    // In this code , the board, rowSize, and winningRowIndices variables 
+    private final int rowSize;                       // are all declared as final, which means that their values cannot be changed once they are set.
     private final List<List<Integer>> winningRowIndices;
 
+    
     public Board() {
         this(BoardMathHelpers.defaultBoard());
     }
