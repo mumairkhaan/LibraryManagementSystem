75ce4f01ab2d0920e94301af3971cfd7a9a29bc1
mumairkhaan <99419823+mumairkhaan@users.noreply.github.com>
2023-03-11 19:17:52 +0100

    Update studentsTest.java

--- LibraryManagamentSystem-main/liberaryManagment/src/test/java/studentsTest.java
+++ LibraryManagamentSystem-main/liberaryManagment/src/test/java/studentsTest.java
@@ -4,6 +4,7 @@ class studentsTest {
 students s=new students();
     @org.junit.jupiter.api.Test
     void isStudent() {
+        
         Student s1 = new Student("1","Muhammad Umair");
         int res=s.isStudent();
         assertEquals(-1,res);
