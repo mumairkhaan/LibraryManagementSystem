8a798b91d1045261d8e75967f95bc79795ffa825
mumairkhaan <99419823+mumairkhaan@users.noreply.github.com>
2023-03-11 19:31:39 +0100

    Update BoardDsl.java

--- Functional Programming/BoardDsl.java
+++ Functional Programming/BoardDsl.java
@@ -1,3 +1,5 @@
+
+// this is an example of a DSL (Domain-Specific Language)
 package com.ybrikman.funky.tictactoe;
 
 import com.google.common.collect.ImmutableList;
@@ -23,3 +25,14 @@ public class BoardDsl {
         return new Board(ImmutableList.<Optional<Player>>builder().add(players).build());
     }
 }
+
+// EXPLANATION:
+
+
+// Specifically, the BoardDsl class provides methods that allow you to create a Board object with a specific configuration of Player objects. 
+// The methods x(), o() and empty() create Optional<Player> objects representing the Player values X, O, and empty, respectively.
+// The board() method takes an array of Optional<Player> objects and returns a Board object that is constructed from 
+// an ImmutableList of those players.
+
+// these methods allow us to create a Board object with a concise and  easy-to-read syntax. this class serves as a DSL for 
+// configuring Board objects in a specific way.
